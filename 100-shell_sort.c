e8fd07e4850b5701f02ff71a6c57d4c1555836c1
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-10-24 13:11:02 -0700

    task 100

---
+++ 100-shell_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+/**
+ * shell_sort - array sorting using shell
+ * @array: array
+ * @size: array size
+ */
+void shell_sort(int *array, size_t size)
+{
+	if (!array || !size)
+	{
+		return;
+	}
+}
