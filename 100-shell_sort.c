259266661e88fb3aff7d40f63183f1d790e221e8
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-10-24 13:18:19 -0700

    task 4

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,14 +1,43 @@
 #include "sort.h"
 
+
 /**
- * shell_sort - array sorting using shell
- * @array: array
- * @size: array size
+ * shell_sort - Sorts an array of integers
+ * @array: Array
+ * @size: Size
  */
 void shell_sort(int *array, size_t size)
 {
-	if (!array || !size)
+	size_t gap = 1;
+	size_t a, b;
+	int buck;
+
+	if (array == NULL || size < 2)
 	{
 		return;
 	}
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
+
+	while (gap > 0)
+	{
+		for (a = gap; a < size; a++)
+		{
+			buck = array[a];
+			b = a;
+
+			while (b >= gap && array[b - gap] > buck)
+			{
+				array[b] = array[b - gap];
+				b -= gap;
+			}
+
+			array[b] = buck;
+		}
+
+		print_array(array, size);
+
+		gap = (gap - 1) / 3;
+	}
 }
+
